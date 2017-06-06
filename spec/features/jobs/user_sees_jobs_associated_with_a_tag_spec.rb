17e3f71bdaa0c586549f5e9b354576cf5d6e816b
Chantal Justamond <cjustamm66@me.com>
2017-06-06 10:31:17 -0600

    user sees tags associated with jobs test added

---
+++ spec/features/jobs/user_sees_jobs_associated_with_a_tag_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "User can see tags associated with a job" do
+  scenario "a user can see tags associated with a job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_jobs_path(company, job)
+
+    expect(page).to have_link("Developer")
+    expect(page).to have_link("Aetna")
+
+    visit company_job_path
+
+    expect(page).to have_content('Software')
+    expect(page).to have_content('Good-Location')
+
+
+
+
+
+
+  end
+
+end
