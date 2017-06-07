fdf67da4aef1b171d27815379d71712ce9339d79
Chantal Justamond <cjustamm66@me.com>
2017-06-07 10:59:16 -0600

    test office location added and commented tags test

---
+++ spec/features/companies/user_can_see_office_locations_assocaited_w_company_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "User can see office locations on a specific company" do
+  scenario "User can see office locations on a specific company" do
+    company = Company.create!(name: "ESPN")
+    location = company.locations.create!(location: 'Denver')
+
+    visit company_path(company)
+
+    expect(page).to have_current_path(company_path(company))
+    expect(page).to have_content(company.name)
+
+
+    # save_and_open_page
+    # binding.pry
+    expect(page).to have_content(location.location)
+  end
+end
